791945bfca86c9a0a7c752be5141d93742f9b403
Steven Roberts <sr320@uw.edu>
2019-11-05 11:00:51 -0800

    new histogram code

--- scripts/lecture.R
+++ scripts/lecture.R
@@ -37,5 +37,6 @@ ggplot(fish_data_cat_largescales, aes(x=length, y= scalelength, color = lakeid))
   geom_point(size=1)
   
   
-  
+  ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram()
 
