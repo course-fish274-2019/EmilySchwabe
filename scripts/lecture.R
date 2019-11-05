50eae4d78889988d3bd312a888f32d9059000d4e
Emily Schwabe <emilyschwabe@Emilys-MacBook-Air-2.local>
2019-11-05 10:25:27 -0800

    added a comment about the type of column that was added

--- scripts/lecture.R
+++ scripts/lecture.R
@@ -3,5 +3,10 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 
+
+
+
+
+#using the fish data and then piping a new colunm for fish bigger than 300
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
