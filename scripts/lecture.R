7c1170dcc00bc8b23fa9b29b949713034923975a
Emily Schwabe <emilyschwabe@Emilys-MacBook-Air-2.local>
2019-11-05 10:31:09 -0800

    filter the scale length fish data that was less than 1 mm

--- scripts/lecture.R
+++ scripts/lecture.R
@@ -10,3 +10,10 @@ library(dplyr)
 #using the fish data and then piping a new colunm for fish bigger than 300
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
+#measure the scale length of the fish in Gaeta_etal_CLC_data.csv is not accurate for those smaller than 1 mm. Use dplyr to remove the fish with a scalelength of less than 1 mm from fish_data_cat. The new dataset will have 4,029 rows.
+
+
+
+fish_data_cat_largescales <- filter(fish_data_cat, scalelength > 1)
